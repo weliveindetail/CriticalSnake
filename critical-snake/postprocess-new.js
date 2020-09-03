7b5b78f30494b6b537f6d5530f1a2ccfa9273efd
Stefan Gränitz <stefan.graenitz@gmail.com>
2020-09-03 15:44:14 +0200

    Refactor internals of jQuery-style component (step 2)

--- critical-snake/postprocess-new.js
+++ critical-snake/postprocess-new.js
@@ -12,70 +12,22 @@ function initialCoordBounds() {
   };
 }
 
-function floatCoord(oldFormat) {
-  let chars = oldFormat.toString().split('');
-  chars.splice(-6, 0, '.');
-  return parseFloat(chars.join(''));
-}
-
-function directionAngleRadians(c1, c2) {
-  const norm = (lat) => Math.tan((lat / 2) + (Math.PI / 4));
-  const Δφ = Math.log(norm(c2.lat) / norm(c1.lat));
-  const Δlon = Math.abs(c1.lng - c2.lng);
-  return Math.atan2(Δlon, Δφ);
-}
-
-function haversineMeters(c1, c2) {
-  const R = 6371e3; // metres
-  const φ1 = c1.lat * Math.PI / 180; // φ, λ in radians
-  const φ2 = c2.lat * Math.PI / 180;
-  const Δφ = (c2.lat - c1.lat) * Math.PI / 180;
-  const Δλ = (c2.lng - c1.lng) * Math.PI / 180;
-
-  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-  return R * c;
-}
-
-function timeDiffSeconds(c1, c2) {
-  const ms = new Date(c2.timestamp * 1000) - new Date(c1.timestamp * 1000);
-  return ms / 1000;
-}
-
-function calculateVector(latest, next) {
-  const radians = directionAngleRadians(latest, next);
-  if (radians < 0 || radians > 2 * Math.PI || isNaN(radians)) {
-    console.warn("Dropping data-point due to invalid direction",
-                 radians, " (radians) from", latest, "to", next);
-    return null;
-  }
-
-  const meters = haversineMeters(latest, next);
-  if (meters <= 0) {
-    console.warn("Dropping data-point due to invalid distance",
-                 meters, "(meters) from", latest, "to", next);
-    return null;
-  }
-
-  const seconds = timeDiffSeconds(latest, next);
-  if (seconds < 0) {
-    console.warn("Dropping data-point due to invalid duration",
-                 seconds, " (seconds) from", latest, "to", next);
-    return null;
-  }
-
-  return {
-    direction: radians,
-    distance: meters,
-    duration: seconds
-  }
-}
-
-
 CriticalSnake.PostProcessor = function(options) {
+
+  const self = this;
+
+  function importApiVersion2DataPoint(dataPoint) {
+    const floatCoord = (oldFormat) => {
+      let chars = oldFormat.toString().split('');
+      chars.splice(-6, 0, '.');
+      return parseFloat(chars.join(''));
+    };
+    return {
+      stamp: new Date(dataPoint.timestamp * 1000),
+      lat: floatCoord(dataPoint.latitude),
+      lng: floatCoord(dataPoint.longitude)
+    };
+  }
 
   function splitTrack(vector) {
     if (vector.duration > options.splitTrack.gapDuration) {
@@ -87,87 +39,130 @@ CriticalSnake.PostProcessor = function(options) {
     return false;
   }
 
+  // Use simple integers as track IDs.
+  function hashToIdx(hash) {
+    if (!self.indexMap.hasOwnProperty(hash)) {
+      // For split-tracks we can have multiple indexes for one participant.
+      self.indexMap[hash] = [ self.nextIndex++ ];
+    }
+    // Latest track-index for the participant.
+    return back(self.indexMap[hash]);
+  }
+
+  function newIdxForHash(hash) {
+    if (!self.indexMap.hasOwnProperty(hash)) {
+      console.error("Invalid use of newIdxForHash()");
+    }
+    self.indexMap[hash].push(self.nextIndex++);
+    return back(self.indexMap[hash]);
+  }
+
+  function directionAngleRadians(c1, c2) {
+    const norm = (lat) => Math.tan((lat / 2) + (Math.PI / 4));
+    const Δφ = Math.log(norm(c2.lat) / norm(c1.lat));
+    const Δlon = Math.abs(c1.lng - c2.lng);
+    return Math.atan2(Δlon, Δφ);
+  }
+
+  function haversineMeters(c1, c2) {
+    const R = 6371e3; // metres
+    const φ1 = c1.lat * Math.PI / 180; // φ, λ in radians
+    const φ2 = c2.lat * Math.PI / 180;
+    const Δφ = (c2.lat - c1.lat) * Math.PI / 180;
+    const Δλ = (c2.lng - c1.lng) * Math.PI / 180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+              Math.cos(φ1) * Math.cos(φ2) *
+              Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return R * c;
+  }
+
+  // The vector is the transition info between the last and the current
+  // data-point in the track.
+  function calculateVector(participant, dataPoint) {
+    if (!options.coordFilter([ dataPoint.lat, dataPoint.lng ])) {
+      self.filteredOutOfRange += 1;
+      return null;
+    }
+
+    const idx = hashToIdx(participant);
+    if (self.tracks.length <= idx) {
+      self.tracks[idx] = [ dataPoint ];
+      return null;
+    }
+
+    const latest = back(self.tracks[idx]);
+    if (latest.stamp == dataPoint.stamp) {
+      self.filteredDupes += 1;
+      return null;
+    }
+
+    // TODO: We should record a min/max timestamp here.
+    if (latest.lat == dataPoint.lat && latest.lng == dataPoint.lng) {
+      self.filteredDupes += 1;
+      return null;
+    }
+
+    if (latest.stamp > dataPoint.stamp) {
+      console.error("Invalid dataset ordering: timestamp", latest.stamp,
+                    "> timestamp", dataPoint.stamp);
+      return null;
+    }
+
+    const radians = directionAngleRadians(latest, dataPoint);
+    if (radians < 0 || radians > 2 * Math.PI || isNaN(radians)) {
+      console.warn("Dropping data-point due to invalid direction",
+                  radians, " (radians) from", latest, "to", dataPoint);
+      return null;
+    }
+
+    const meters = haversineMeters(latest, dataPoint);
+    if (meters <= 0) {
+      console.warn("Dropping data-point due to invalid distance",
+                  meters, "(meters) from", latest, "to", dataPoint);
+      return null;
+    }
+
+    const seconds = (dataPoint.stamp - latest.stamp) / 1000;
+    if (seconds < 0) {
+      console.warn("Dropping data-point due to invalid duration",
+                  seconds, " (seconds) from", latest, "to", dataPoint);
+      return null;
+    }
+
+    return {
+      direction: radians,
+      distance: meters,
+      duration: seconds
+    }
+  }
+
   this.run = (dataset) => {
-
-    // Use simple integers as track IDs.
-    let nextIndex = 0;
-    let indexMap = {};
-    const hashToIdx = (hash) => {
-      if (!indexMap.hasOwnProperty(hash)) {
-        // For split-tracks we can have multiple indexes for one participant.
-        indexMap[hash] = [ nextIndex++ ];
-      }
-      // Latest track-index for the participant.
-      return back(indexMap[hash]);
-    };
-    const newIdxForHash = (hash) => {
-      if (!indexMap.hasOwnProperty(hash)) {
-        console.error("Invalid use of newIdxForHash()");
-      }
-      indexMap[hash].push(nextIndex++);
-      return back(indexMap[hash]);
-    };
-
-    // Make a tracks of data points by ID and deduplicate entries.
-    const tracks = [];
-    const addToTrack = (participant, dataPoint) => {
-      if (!options.coordFilter([ dataPoint.lat, dataPoint.lng ])) {
-        this.filteredOutOfRange += 1;
-        return;
-      }
-
-      const idx = hashToIdx(participant);
-      if (tracks.length <= idx) {
-        tracks[idx] = [ dataPoint ];
-        return;
-      }
-
-      const latest = back(tracks[idx]);
-      if (latest.timestamp == dataPoint.timestamp) {
-        this.filteredDupes += 1;
-      }
-      else if (latest.lat == dataPoint.lat && latest.lng == dataPoint.lng) {
-        // TODO: We should record a min/max timestamp here.
-        this.filteredDupes += 1;
-      }
-      else if (latest.timestamp > dataPoint.timestamp) {
-        console.error("Invalid dataset ordering: timestamp", latest.timestamp,
-                      "> timestamp", dataPoint.timestamp);
-      }
-      else {
-        // The vecotr is the transition info between the last and the current data-point.
-        const vector = calculateVector(latest, dataPoint);
-        if (vector == null) {
-          // Drop data-point.
-          return;
-        }
-
-        if (splitTrack(vector)) {
-          // Drop the vector and create a new track for this data-point.
-          const newIdx = newIdxForHash(participant);
-          tracks[newIdx] = [ dataPoint ];
-        }
-        else {
-          // Add vector nd data-point to the current track.
-          latest.vector = vector;
-          tracks[idx].push(dataPoint);
-        }
-      }
-    };
 
     // Dataset pass: populate tracks
     for (const snapshot in dataset) {
       for (const participant in dataset[snapshot]) {
-        const dataPoint = dataset[snapshot][participant];
-        addToTrack(participant, {
-          timestamp: dataPoint.timestamp,
-          lat: floatCoord(dataPoint.latitude),
-          lng: floatCoord(dataPoint.longitude)
-        });
+        const dataPoint = importApiVersion2DataPoint(dataset[snapshot][participant]);
+        const vector = calculateVector(participant, dataPoint);
+
+        if (vector) {
+          if (splitTrack(vector)) {
+            // Drop the vector and create a new track for this data-point.
+            const idx = newIdxForHash(participant);
+            self.tracks[idx] = [ dataPoint ];
+          }
+          else {
+            // Add vector to the latest data-point in the track and push the new
+            // data-point on top.
+            const idx = hashToIdx(participant);
+            back(self.tracks[idx]).vector = vector;
+            self.tracks[idx].push(dataPoint);
+          }
+        }
       }
     }
-
-    console.log(tracks);
 
     return {
       origin: [52.5, 13.4],
@@ -175,12 +170,13 @@ CriticalSnake.PostProcessor = function(options) {
       frames: [{
         coord: [52.51, 13.41],
         snake: null
-      }],
-      tracks: tracks
+      }]
     };
   }; // CriticalSnake.PostProcessor.run()
 
-
+  this.tracks = [];
+  this.indexMap = {};
+  this.nextIndex = 0;
   this.filteredDupes = 0;
   this.filteredOutOfRange = 0;
 
